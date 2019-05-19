c79385a29b06f850e068e49f906cb935fe364837
Nimat Azeez <nimat@nimats-macbook-pro.local>
2019-05-18 19:20:14 -0700

    Continue work on TimeBlockTableView with the addition of the CustomBlockTableCell. Committing now to test play around with code in attempt to fix a bug.

--- Block Pro/Controller/TimeBlockViewController.swift
+++ Block Pro/Controller/TimeBlockViewController.swift
@@ -26,53 +26,61 @@ class TimeBlockViewController: UIViewController, UITableViewDelegate, UITableVie
         timeTableView.delegate = self
         timeTableView.dataSource = self
         timeTableView.showsVerticalScrollIndicator = false
-        //timeTableView.allowsSelection = false
+        timeTableView.allowsSelection = false
         timeTableView.separatorStyle = .none
         timeTableView.rowHeight = 80.0
         
         blockTableView.delegate = self
         blockTableView.dataSource = self
-        blockTableView.separatorStyle = .none
+        //blockTableView.separatorStyle = .none
         blockTableView.rowHeight = 80.0
         
-        verticalTableViewSeperator.layer.cornerRadius = 0.4 * verticalTableViewSeperator.bounds.size.width
+        verticalTableViewSeperator.layer.cornerRadius = 0.5 * verticalTableViewSeperator.bounds.size.width
         verticalTableViewSeperator.clipsToBounds = true
         
         timeTableView.register(UINib(nibName: "CustomTimeTableCell", bundle: nil), forCellReuseIdentifier: "timeCell")
+        //blockTableView.register(UINib(nibName: "CustomBlockTableCell", bundle: nil), forCellReuseIdentifier: "blockCell")
         
     }
 
+    //MARK: - TableView Datasource Methods
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         if tableView == timeTableView {
             return cellTimes.count
         }
-        else if tableView == blockTableView {
-            return cellTimes2.count
-        }
-        
         else {
-            return 0
-            
-    }
+            return 1
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        if tableView == timeTableView {
+        //if tableView == timeTableView {
             let cell = tableView.dequeueReusableCell(withIdentifier: "timeCell", for: indexPath) as! CustomTimeTableCell
-            cell.timeLabel.adjustsFontSizeToFitWidth = true
+            cell.timeLabel.font = UIFont(name: "Helvetica Neue", size: 9)
             cell.timeLabel.text = cellTimes[indexPath.row]
-
+            
+            if cell.timeLabel.text == "11:00 PM" {
+                cell.cellSeperator.backgroundColor = UIColor.white
+            }
+            else {
+                cell.cellSeperator.backgroundColor = UIColor.black
+            }
             return cell
-        }
-        else {
-            let cell2 = tableView.dequeueReusableCell(withIdentifier: "Cell2", for: indexPath)
-            cell2.textLabel?.text = cellTimes2[indexPath.row]
-            return cell2
-        }
+        //}
+//        else {
+//            let cell = tableView.dequeueReusableCell(withIdentifier: "blockCell", for: indexPath) as! CustomBlockTableCell
+//            //cell.textLabel?.text = "None"
+//            return cell
+//        }
+
     }
+    
+    
+    //MARK: - TableView Delegate Methods
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         
@@ -86,10 +94,13 @@ class TimeBlockViewController: UIViewController, UITableViewDelegate, UITableVie
         }
     }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let cell = tableView.cellForRow(at: indexPath)
-        
-        print (cell?.frame.origin.y)
-    }
+    
+    
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "timeCell", for: indexPath) as! CustomTimeTableCell
+//
+//        tableView.deselectRow(at: indexPath, animated: true)
+//        print (cell.timeLabel.font)
+//    }
 }
 
---
+++ Block Pro/View/Custom Cells/CustomBlockTableCell.swift
@@ -0,0 +1,27 @@
+//
+//  CustomBlockTableCell.swift
+//  Block Pro
+//
+//  Created by Nimat Azeez on 5/18/19.
+//  Copyright © 2019 Nimat Azeez. All rights reserved.
+//
+
+import UIKit
+
+class CustomBlockTableCell: UITableViewCell {
+
+    
+    @IBOutlet weak var editButton: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
